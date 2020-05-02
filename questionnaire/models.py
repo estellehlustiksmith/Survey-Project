b0ea6e99f3037fe49fdb256989eb646385145049
estellehlustiksmith <64281339+estellehlustiksmith@users.noreply.github.com>
2020-05-02 19:12:00 +0100

    Can save resposes!

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Response(models.Model):
-    question1 = models.CharField(blank=True, max_length=1028)
-    question2 = models.CharField(blank=True, max_length=1028)
-    question3 = models.CharField(blank=True, max_length=1028)
+    question1 = models.CharField(max_length=1028)
+    question2 = models.CharField(max_length=1028)
+    question3 = models.CharField(max_length=1028)
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from .forms import ResponseForm
 
 def response_form(request):
-    form = ResponseForm()
+    #form = ResponseForm()
+    if request.method == "POST":
+        form = ResponseForm(request.POST)
+        if form.is_valid():
+            questionnaire = form.save(commit=False)
+            questionnaire.save()
+            #will need a redirect to a thank you page
+    else:
+        form = ResponseForm()
     return render(request, 'questionnaire/response_form.html', {'form': form})
 
 # Create your views here.
