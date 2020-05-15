ab1f9249baa56fca3384e2a7aaf82e2de318d4f7
estellehlustiksmith <64281339+estellehlustiksmith@users.noreply.github.com>
2020-05-15 11:56:54 +0100

    Made checkboxes

--- questionnaire/forms.py
+++ questionnaire/forms.py
@@ -1,7 +1,22 @@
 from django import forms
 from .models import Response
+from .models import Consent
+
 class ResponseForm(forms.ModelForm):
     class Meta:
         model = Response
         fields = ('question1','question2','question3', 'drawing')
-        widgets = {'drawing': forms.HiddenInput()}
+        widgets = {'drawing': forms.HiddenInput()}#
+
+class Consent(forms.ModelForm):
+    consent1 = forms.BooleanField(required = True)
+    consent2 = forms.BooleanField(required = True)
+    consent3 = forms.BooleanField(required = True)
+    consent4 = forms.BooleanField(required = True)
+    consent5 = forms.BooleanField(required = True)
+    consent6 = forms.BooleanField(required = True)
+    consent7 = forms.BooleanField(required = True)
+    consent8 = forms.BooleanField(required = True)
+    class Meta:
+        model = Consent
+        fields=('consent1','consent2','consent3','consent4','consent5','consent6','consent7','consent8')
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -7,3 +7,13 @@ class Response(models.Model):
     question3 = models.CharField(max_length=1028)
 
     drawing = models.CharField(max_length=1000000000)
+
+class Consent(models.Model):
+    consent1 = models.BooleanField()
+    consent2 = models.BooleanField()
+    consent3 = models.BooleanField()
+    consent4 = models.BooleanField()
+    consent5 = models.BooleanField()
+    consent6 = models.BooleanField()
+    consent7 = models.BooleanField()
+    consent8 = models.BooleanField()
