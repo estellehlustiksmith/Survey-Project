f71bc4fbb7ca14a650e3fc1d86cb9f0f8be08a20
estellehlustiksmith <64281339+estellehlustiksmith@users.noreply.github.com>
2020-07-18 16:36:46 +0100

    added interview_q js

---
+++ questionnaire/static/js/interview_q_script.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").style.display= "none";
+
+function Int1() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.getElementById("submit").style.display= "block";
+    document.getElementById("int1_btn").style.display= "none";
+}
